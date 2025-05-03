fe1998b16cc7858a269cb1b62cf97889dece7287
@pradip <pradeep.kathar@insigniaconsultancy.com>
2025-05-03 19:11:18 +0530

    multiple routes

--- src/app.js
+++ src/app.js
@@ -2,18 +2,33 @@ const express = require("express");
 
 const app = express ();
 
-app.use("/", (req, res)=>{
-     res.send("Hello ==>")
-} );
 
-app.use("/hello",(req,res)=>{
-    res.send(" Hello is here");
+app.get("/user",(req,res) => {
+    console.log(req.query);
+    res.send({FN:"pk", LN:"KK"});
 });
 
-app.use("/test",(req,res)=>{
-    res.send("I am from test");
+app.get("/user/:userId",(req,res) => {
+    console.log(req.params)
+    res.send({userid: 1, fn: "pp"});
 })
 
-app.listen(3000, () => {
-    console.log("Server Successfully response port no 3000");
+app.get("/rount",(req,res,next) => {
+    console.log("rount 1");
+    // res.send("rount 1 through Response")
+    next()
+},(req,res,next) => {
+    console.log("round 2");
+    // res.send("rount 2 through Response")
+    next()
+},(req,res,next) => {
+    console.log("round 3");
+    res.send("rount 3 through Response")
+},(req,res) => {
+    console.log("round 4")
+    res.send("rount 4 through Response")
+})
+
+app.listen(4000, () => {
+    console.log("Server Successfully response port no 4000");
 })
