c79ebd5861b525e1d0fbd3ae6da744ef5e11d7fb
@pradip <pradeep.kathar@insigniaconsultancy.com>
2025-05-18 00:10:15 +0530

    Schema validation

--- src/app.js
+++ src/app.js
@@ -1,87 +1,83 @@
 const express = require("express");
 const { connectDB } = require("./config/database");
 const User = require("./models/user");
-const app = express()
+const app = express();
 
 app.use(express.json());
 
-app.post("/signup",async(req,res) => {
-    
-    const user = new User(req.body);
-    //Creating a new instance of the User model
+app.post("/signup", async (req, res) => {
+  const user = new User(req.body);
+  //Creating a new instance of the User model
 
-    // const user = new User({
-    //      firstName : "nana",
-    //      lastName : "patekar",
-    //      emailId : "nana@gmail.com",
-    //      password : "nana123",
-    // });
+  // const user = new User({
+  //      firstName : "nana",
+  //      lastName : "patekar",
+  //      emailId : "nana@gmail.com",
+  //      password : "nana123",
+  // });
 
-    try{
-        await user.save();
-        res.send("User Added successfully");
-    } catch (err){
-        res.status(400).send("Error saving the user:" + err.message);
+  try {
+    await user.save();
+    res.send("User Added successfully");
+  } catch (err) {
+    res.status(400).send("Error saving the user:" + err.message);
+  }
+});
+
+app.get("/user", async (req, res) => {
+  const userEmail = req.body.emailId;
+  try {
+    const users = await User.find({ emailId: userEmail });
+    if (users.length === 0) {
+      res.status(400).send("Something went wrong");
+    } else {
+      res.send(users);
     }
-})
+  } catch (err) {
+    res.status(400).send("Error fetching users");
+  }
+});
 
-app.get("/user",async(req,res)=>{
-    const userEmail = req.body.emailId;
-    try{
-        const users = await User.find({emailId : userEmail});
-        if(users.length === 0){
-            
-            res.status(400).send("Something went wrong");
-        }else{
-            res.send(users);
-        }
-    }catch(err){
-        res.status(400).send("Error fetching users");
-    }
-})
+app.get("/feed", async (req, res) => {
+  try {
+    const users = await User.find();
+    res.send(users);
+  } catch (err) {
+    res.status(400).send("Error fetching users");
+  }
+});
 
-app.get("/feed",async(req,res) => {
-    try{
-     const users = await User.find();
-     res.send(users);
-    }catch(err){
-        res.status(400).send("Error fetching users");
-    }
-})
+app.delete("/user", async (req, res) => {
+  const userId = req.body.userId;
+  try {
+    const user = await User.findByIdAndDelete(userId);
+    res.send("User Successfully Deleted");
+  } catch (err) {
+    res.status(400).send("Error fetching users");
+  }
+});
 
-app.delete("/user", async(req,res)=>{
-    const userId = req.body.userId;
-    try{
-        const user = await User.findByIdAndDelete(userId);
-        res.send("User Successfully Deleted")
-    }catch(err){
-        res.status(400).send("Error fetching users");
-    }
-})
-
-app.patch("/user",async(req,res)=>{
-       const userId = req.body.userId;
-       const data = req.body;
-       try{
-        const user = await User.findByIdAndUpdate({_id : userId}, data);
-        res.send("User Successfully update");
-       }catch(err){
-        res.status(400).send("Error fetching users")
-       }
-;})
-
-
+app.patch("/user", async (req, res) => {
+  const userId = req.body.userId;
+  const data = req.body;
+  try {
+    const user = await User.findByIdAndUpdate({ _id: userId }, data, {
+      returnDocument: "after",
+      runValidators: true,
+    });
+    res.send("User Successfully update");
+  } catch (err) {
+    res.status(400).send("UPDATE FAILED:" + err.message);
+  }
+});
 
 connectDB()
-.then(() =>{
+  .then(() => {
     console.log("Database connected");
     app.listen(4000, () => {
-        console.log("Server Successfully response port no 4000");
-      });
-})
-.catch((err) => {
+      console.log("Server Successfully response port no 4000");
+    });
+  })
+  .catch((err) => {
     console.log("Database is not connected");
-})
-
-
-
+  });
--- src/models/user.js
+++ src/models/user.js
@@ -1,24 +1,52 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-const userSchema = new mongoose.Schema({
-    firstName : {
-        type : String,
+const userSchema = new mongoose.Schema(
+  {
+    firstName: {
+      type: String,
+      required: true,
+      minLength: true,
+      maxLength: 50,
     },
-    lastName : {
-        type : String,
+    lastName: {
+      type: String,
     },
-    emailId : {
-        type: String,
+    emailId: {
+      type: String,
+      unique: true,
+      lowercase: true,
+      trim: true,
+      required: true,
     },
-    password : {
-        type : String,
+    password: {
+      type: String,
+      required: true,
     },
-    age : {
-        type : Number,
+    age: {
+      type: Number,
+      min: 18,
     },
-    gender : {
-        type : String,
-        },
-    });
+    gender: {
+      type: String,
+      validate(value) {
+        if (!["male", "famale", "other"].includes(value)) {
+          throw new Error("Gender data is not valid");
+        }
+      },
+    },
+    photoUrl: {
+      type: String,
+      default:
+        "https://en.wikipedia.org/wiki/File:Person_%281102860%29_-_The_Noun_Project.svg",
+    },
+    about: {
+      type: String,
+      default: "Hi i am default value ",
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
 
-    module.exports = mongoose.model('User', userSchema);  //exporting the schema as a model
+module.exports = mongoose.model("User", userSchema); //exporting the schema as a model
