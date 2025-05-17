9467cf01435eca2596da26a6170235d6ddd106d7
@pradip <pradeep.kathar@insigniaconsultancy.com>
2025-05-17 23:43:18 +0530

    Delete and patch API created

--- src/app.js
+++ src/app.js
@@ -49,6 +49,27 @@ app.get("/feed",async(req,res) => {
     }
 })
 
+app.delete("/user", async(req,res)=>{
+    const userId = req.body.userId;
+    try{
+        const user = await User.findByIdAndDelete(userId);
+        res.send("User Successfully Deleted")
+    }catch(err){
+        res.status(400).send("Error fetching users");
+    }
+})
+
+app.patch("/user",async(req,res)=>{
+       const userId = req.body.userId;
+       const data = req.body;
+       try{
+        const user = await User.findByIdAndUpdate({_id : userId}, data);
+        res.send("User Successfully update");
+       }catch(err){
+        res.status(400).send("Error fetching users")
+       }
+;})
+
 
 
 connectDB()
