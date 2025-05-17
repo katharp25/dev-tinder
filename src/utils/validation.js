b8621ddd5f8ba7a2e518ad1c96adfd3e4531e7b3
@pradip <pradeep.kathar@insigniaconsultancy.com>
2025-05-18 03:03:57 +0530

    created loginAPI and increpted password

---
+++ src/utils/validation.js
@@ -0,0 +1,18 @@
+const validator = require("validator");
+
+const validateSignUpData = (req) => {
+  const { firstName, lastName, emailId, password } = req.body;
+  if (!firstName || !lastName) {
+    throw new Error("First name and last name are required");
+  } else if (!validator.isEmail(emailId)) {
+    throw new Error("Invalid email");
+  } else if (!validator.isStrongPassword(password)) {
+    throw new Error(
+      "Password should be at least 8 characters long, and should contain at least"
+    );
+  }
+};
+
+module.exports = {
+  validateSignUpData,
+};
