06122f7477c1447206924af816265c30fc3654d0
@pradip <pradeep.kathar@insigniaconsultancy.com>
2025-05-21 09:18:30 +0530

    create JWT token and use /profile

--- src/app.js
+++ src/app.js
@@ -4,25 +4,27 @@ const User = require("./models/user");
 const { validateSignUpData } = require("./utils/validation");
 const bcrypt = require("bcrypt");
 const app = express();
+const cookieParsar = require("cookie-parser");
+const jwt = require("jsonwebtoken");
 
 app.use(express.json());
+app.use(cookieParsar());
 
 app.post("/signup", async (req, res) => {
   try {
     // Validation of data
     validateSignUpData(req);
-    const {firstName, lastName, emailId, password} = req.body;
+    const { firstName, lastName, emailId, password } = req.body;
     //Encrypt the password
     const passwordHash = await bcrypt.hash(password, 10);
     console.log(passwordHash);
 
-
     // Creating a new instance of the User model
     const user = new User({
-        firstName,
-        lastName,
-        emailId,
-        password: passwordHash
+      firstName,
+      lastName,
+      emailId,
+      password: passwordHash,
     });
     await user.save();
     res.send("User Added successfully");
@@ -31,23 +33,53 @@ app.post("/signup", async (req, res) => {
   }
 });
 
-app.post("/login", async(req,res) => {
-    try {
-           const {emailId, password} = req.body;
-           const user = await User.findOne({emailId});
-           if(!user){
-            throw new Error("Invalid credentials");
-           }
-           const isValidPassword = await bcrypt.compare(password, user.password);
-           if(isValidPassword){
-            res.send("Login Succesfully!!!")
-           }else{
-            throw new Error(" Invalid credentials");   
-           }
-
-    }catch (err){
-        res.status(400).send("ERROR :" + err.message);
+app.post("/login", async (req, res) => {
+  try {
+    const { emailId, password } = req.body;
+    const user = await User.findOne({ emailId });
+    if (!user) {
+      throw new Error("Invalid credentials");
     }
+    const isValidPassword = await bcrypt.compare(password, user.password);
+    if (isValidPassword) {
+      //Create a JWT Token
+      const token = await jwt.sign({ _id: user._id }, "pk25/11/1994");
+      console.log(token);
+
+      // Add the token to cookie and send the response back to the user
+
+      res.cookie("token", token);
+      res.send("Login Succesfully!!!");
+    } else {
+      throw new Error(" Invalid credentials");
+    }
+  } catch (err) {
+    res.status(400).send("ERROR :" + err.message);
+  }
+});
+
+app.get("/profile", async (req, res) => {
+  try {
+    const cookies = req.cookies;
+
+    const { token } = cookies;
+    if (!token) {
+      throw new Error("Invalid Token");
+    }
+
+    // Validate my token
+
+    const decodedMessage = await jwt.verify(token, "pk25/11/1994");
+    const { _id } = decodedMessage;
+    const user = await User.findById(_id);
+    if (!user) {
+      throw new Error("User Does Not Exist");
+    }
+
+    res.send(user);
+  } catch (err) {
+    res.status(400).send("ERROR :" + err.message);
+  }
 });
 
 app.get("/user", async (req, res) => {
